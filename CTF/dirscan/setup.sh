74201fe9d962dbbec29e678f3ace425be956ca16
xrchsploit <54417402+xrchsploit@users.noreply.github.com>
2020-04-30 10:38:09 -0500

    Add files via upload

---
+++ CTF/dirscan/setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install gobuster -y
+
+sudo chmod +x gob
+
+git -C . clone https://github.com/xrchsploit/dirlist
+
+echo"[*] Usage: ./gob"
---
+++ CTF/portscan/setup.sh
@@ -0,0 +1,3 @@
+sudo apt-get install nmap
+
+sudo chmod +x port
